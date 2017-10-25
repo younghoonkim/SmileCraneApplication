2eb0a565d40e75f94bf70cd3b3192a3e06a83d1c
김병하 <byungkim@ebay.com>
2017-10-25 16:11:58 +0900

    activity에 이미지 추가

--- app/src/main/java/com/ebaykorea/smilecraneapplication/smilecraneapplication/FifthActivity.java
+++ app/src/main/java/com/ebaykorea/smilecraneapplication/smilecraneapplication/FifthActivity.java
@@ -5,17 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class FourthActivity extends AppCompatActivity {
+public class FifthActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fourth);
+        setContentView(R.layout.activity_fifth);
     }
 
     public void onClick(View view)
     {
-        Intent intent = new Intent(this, FifthActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
