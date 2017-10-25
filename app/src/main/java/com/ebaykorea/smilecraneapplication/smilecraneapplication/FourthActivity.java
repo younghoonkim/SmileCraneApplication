084452b4d662c512c77fede61b2f27de48864f07
김병하 <byungkim@ebay.com>
2017-10-25 15:20:58 +0900

    add activities

---
+++ app/src/main/java/com/ebaykorea/smilecraneapplication/smilecraneapplication/FourthActivity.java
@@ -0,0 +1,22 @@
+package com.ebaykorea.smilecraneapplication.smilecraneapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FourthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth);
+    }
+
+    public void onClick(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
